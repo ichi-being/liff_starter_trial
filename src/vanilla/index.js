3919011cdefe2b52dbc98d8161e18acd743c13cd
ichi_being <dchisbvbph@gmail.com>
2024-01-13 15:01:18 +0900

    起動画面の情報取得、外部ブラウザの場合のログイン、IDtokenの取得

--- src/vanilla/index.js
+++ src/vanilla/index.js
@@ -5,7 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
   liff
     .init({ liffId: process.env.LIFF_ID })
     .then(() => {
-        console.log("Success! you can do something with LIFF API here.")
+        // LIFFアプリが起動された画面の情報を取得
+        const context = liff.getContext();
+        console.log(context);
+        if (!liff.isLoggedIn()) {
+            liff.login({ redirectUri: "https://liffdssample.netlify.app/" });
+        }
+        const idToken = liff.getIDToken();
+        console.log(idToken); // print raw idToken object
     })
     .catch((error) => {
         console.log(error)
